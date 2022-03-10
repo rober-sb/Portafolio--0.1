7c5f38668272373ca0d496478f96259ff84d6a74
Eliancito <eliansilva565@gmail.com>
2022-03-10 07:11:48 -0300

    Creacion de un portafolio solo con html y css (nativo)

---
+++ sweetAlert.js
@@ -0,0 +1,11 @@
+function objetivos(){	
+	Swal.fire({
+
+		 html:'<html><head><script src="https://kit.fontawesome.com/82757a105c.js" crossorigin="anonymous"></script></head><body><h1>Objetivos</h1><br> <h2>Metas diarias</h2>	<p class="fas fa-arrow-alt-circle-right"> Disciplina </p><br><p class="fas fa-arrow-alt-circle-right"> Organizar mi semana </p><br><h3>Objetivo: </h3><p>Ser mas disciplinado</p> <br> <h2>Metas mensuales</h2>	<p class="fas fa-arrow-alt-circle-right"> Enfoque y resiliencia </p><br><p class="fas fa-arrow-alt-circle-right"> Observar mejoras </p><br><h3>Objetivo: </h3><p> Lograr ver cambios y motivarme </p> <br><br> <h2>Metas anuales</h2>	<p class="fas fa-arrow-alt-circle-right"> Vision y sentido </p><br><p class="fas fa-arrow-alt-circle-right"> Mejora a largo plazo </p><br><h3>Objetivo: </h3><p>Desarrollo y progreso personal</p> </body> </html>'
+	});
+}
+function aspiraciones(){	
+	Swal.fire({
+		 html:'<html><head><script src="https://kit.fontawesome.com/82757a105c.js" crossorigin="anonymous"></script></head><body><h1>Aspiraciones</h1><br><br> <p class="fas fa-arrow-alt-circle-right"> Tener una empresa propia de hardware</p><br><br><p class="fas fa-arrow-alt-circle-right"> Vivir en el extranjero </p><br><br><p class="fas fa-arrow-alt-circle-right"> Ayudar a eliminar el socialismo cultural de mi país </p><br><br><p class="fas fa-arrow-alt-circle-right"> Invertir en mi pais y generar trabajo con mi empresa </p><br><br><p class="fas fa-arrow-alt-circle-right"> Hacer de la tecnologia y la automatizacion un alivio a los trabajos pesados </p><br><br><p class="fas fa-arrow-alt-circle-right"> Dar siempre todo de mi en cada actividad que realice </p><br><br><p class="fas fa-arrow-alt-circle-right"> Aprender todo lo que pueda sobre tecnología y más </p><br><br><p class="fas fa-arrow-alt-circle-right"> Inspirar a otros que es posible lograr lo que uno sueña </p></body></html>'
+	});
+}
